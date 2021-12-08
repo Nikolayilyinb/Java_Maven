c5928c58ec22370588f066fbd235d0dde2b6195b
ilin.nikolai <ilin.nikolai@otr.ru>
2021-12-08 21:27:36 +0300

    Practical Stepik task 1

---
+++ src/main/java/org/example/Stepik_Task1.java
@@ -0,0 +1,16 @@
+    /*Напишите программу, выводящую в консоль It's alive! It's alive!
+    Класс должен называться "Main", это ограничение проверяющей системы.
+
+    Sample Input:
+
+    Sample Output:
+    It's alive! It's alive!
+    */
+
+package org.example;
+
+class Stepik_Task1 {
+    public static void main(String[] args) {
+        System.out.print("It's alive! It's alive!");
+    }
+}
