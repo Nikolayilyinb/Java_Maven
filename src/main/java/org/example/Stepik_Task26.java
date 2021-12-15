0b9448f6afc52324868906cb8e13ab346b6ede65
ilin.nikolai <ilin.nikolai@otr.ru>
2021-12-15 22:00:39 +0300

    Practical Stepik task 22-26

---
+++ src/main/java/org/example/Stepik_Task26.java
@@ -0,0 +1,50 @@
+/**
+ *  Дан сериализуемый класс Animal:
+ *
+ * class Animal implements Serializable {
+ *     private final String name;
+ *
+ *     public Animal(String name) {
+ *         this.name = name;
+ *     }
+ *
+ *     @Override
+ *     public boolean equals(Object obj) {
+ *         if (obj instanceof Animal) {
+ *             return Objects.equals(name, ((Animal) obj).name);
+ *         }
+ *         return false;
+ *     }
+ * }
+ * Реализуйте метод, который из переданного массива байт восстановит массив объектов Animal.
+ * Массив байт устроен следующим образом. Сначала идет число типа int, записанное при помощи ObjectOutputStream.writeInt(size).
+ * Далее подряд записано указанное количество объектов типа Animal, сериализованных при помощи ObjectOutputStream.writeObject(animal).
+ *
+ * Если вдруг массив байт не является корректным представлением массива экземпляров Animal,
+ * то метод должен бросить исключение java.lang.IllegalArgumentException.
+ *
+ * Причины некорректности могут быть разные. Попробуйте подать на вход методу разные некорректные данные и посмотрите,
+ * какие исключения будут возникать. Вот их-то и нужно превратить в IllegalArgumentException и выбросить. Если что-то забудете, то проверяющая система подскажет.
+ * Главное не глотать никаких исключений, т.е. не оставлять нигде пустой catch.
+ */
+
+package org.example;
+
+public class Stepik_Task26 {
+    public static Animal[] deserializeAnimalArray(byte[] data) throws IllegalArgumentException{
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
+        try{
+            try(ObjectInputStream ois = new ObjectInputStream(byteStream)){
+                int count = ois.readInt();
+                Animal[] res = new Animal[count];
+                for (int i = 0; i < count; i++){
+                    res[i] =  (Animal)ois.readObject();
+                }
+                return res;
+            }
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException(e);
+        }
+    }
+}
